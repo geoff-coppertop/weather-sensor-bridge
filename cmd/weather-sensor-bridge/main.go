e882bfb6ebd7c6b5c922a64f2b926c2795a642cc
Geoffrey Thomas <geoff.coppertop@gmail.com>
2021-07-21 16:18:19 -0600

    Execute long running process and communicate
    WIP

--- cmd/weather-sensor-bridge/main.go
+++ cmd/weather-sensor-bridge/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -24,6 +27,34 @@ func main() {
 
 	log.Debug(cfg)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "./cmd/weather-sensor-bridge/test.sh")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stdoutDone := make(chan interface{})
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		r := bufio.NewReader(stdout)
+		s, e := Readln(r)
+		for e == nil {
+			log.Println(s)
+			s, e = Readln(r)
+		}
+
+		log.Info(e)
+		log.Info("done")
+		close(stdoutDone)
+	}()
+
 	// Messages will be handled through the callback so we really just need to wait until a shutdown
 	// is requested
 	sig := make(chan os.Signal, 1)
@@ -35,4 +66,29 @@ func main() {
 	<-sig
 
 	log.Info("signal caught - exiting")
+
+	cancel()
+
+	log.Info("cancelled")
+
+	<-stdoutDone
+
+	log.Info("done for real")
+}
+
+// Readln returns a single line (without the ending \n)
+// from the input buffered reader.
+// An error is returned iff there is an error with the
+// buffered reader.
+func Readln(r *bufio.Reader) (string, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return string(ln), err
 }
---
+++ cmd/weather-sensor-bridge/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while [ true ]
+do
+  echo '{"gh_username":"geoff-coppertop", "gateway_ip":"192.168.1.1", "username":"thomasga", "run_rpi_boot":"false", "os_choice":"RaspiOSLite"}'
+  sleep 15
+done
+
+echo 'All done'
